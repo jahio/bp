63fc20baa29127c1e55e032d3ac7902021b1376d
J. Austin Hughey <jah@jah.io>
2020-09-23 18:31:35 -0600

    auto migrate entries table

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import(
   "fmt"
+  "gorm.io/gorm"
+  "gorm.io/driver/sqlite"
 )
 
+type Entry struct {
+  gorm.Model
+  Diastolic  uint
+  Systolic   uint
+  Heartrate  uint
+}
+
 func main() {
-  fmt.Println("Hello World")
+  fmt.Println("Beginning database automigrate...")
+  db, err := gorm.Open(sqlite.Open("bp.db"), &gorm.Config{})
+  if err != nil {
+    panic("Cannot open/create SQLite database bp.db!")
+  }
+
+  db.AutoMigrate(&Entry{})
 }
