6953a8c593ad036a2db3a288d37ccfe78ffeb364
J. Austin Hughey <jah@jah.io>
2020-09-23 18:53:24 -0600

    get basic http working

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module jahio/bp
 go 1.15
 
 require (
+	github.com/gorilla/mux v1.8.0 // indirect
 	gorm.io/driver/sqlite v1.1.3 // indirect
 	gorm.io/gorm v1.20.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import(
   "fmt"
+  "log"
+  "time"
+  "net/http"
   "gorm.io/gorm"
   "gorm.io/driver/sqlite"
+  "github.com/gorilla/mux"
 )
 
 type Entry struct {
@@ -11,6 +15,11 @@ type Entry struct {
   Diastolic  uint
   Systolic   uint
   Heartrate  uint
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+  w.WriteHeader(http.StatusOK)
+  fmt.Fprintf(w, "{ \"status\":\"OK\" }")
 }
 
 func main() {
@@ -21,4 +30,16 @@ func main() {
   }
 
   db.AutoMigrate(&Entry{})
+
+  // Set up HTTP routing for requests/responses
+  r := mux.NewRouter()
+  r.HandleFunc("/", HomeHandler)
+  srv := &http.Server{
+    Handler: r,
+    Addr:    "0.0.0.0:9000",
+    WriteTimeout: 15 * time.Second,
+    ReadTimeout:  15 * time.Second,
+  }
+  
+  log.Fatal(srv.ListenAndServe())
 }
