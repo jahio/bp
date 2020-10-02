613744a0f0401364ec1efef8cd541fd748033d0a
J. Austin Hughey <jah@jah.io>
2020-10-02 14:31:00 -0600

    use json.Number so api doesn't fail on json strings for numbers

--- api/models/entry.go
+++ api/models/entry.go
@@ -4,18 +4,17 @@ import (
 	"encoding/json"
 	"github.com/gobuffalo/pop/v5"
 	"github.com/gobuffalo/validate"
-	"github.com/gobuffalo/validate/validators"
 	"github.com/gofrs/uuid"
 )
 
 // Entry is used by pop to map your entries database table to your go code.
 type Entry struct {
-	ID        uuid.UUID `json:"id"         db:"id"`
-	CreatedAt int       `json:"created_at" db:"created_at"`
-	UpdatedAt int       `json:"updated_at" db:"updated_at"`
-	Systolic  int       `json:"systolic"   db:"systolic"  binding:"required"`
-	Diastolic int       `json:"diastolic"  db:"diastolic" binding:"required"`
-	Heartrate int       `json:"heartrate"  db:"heartrate" binding:"required"`
+	ID        uuid.UUID         `json:"id"         db:"id"`
+	CreatedAt int               `json:"created_at" db:"created_at"`
+	UpdatedAt int               `json:"updated_at" db:"updated_at"`
+	Systolic  json.Number       `json:"systolic"   db:"systolic"  binding:"required"`
+	Diastolic json.Number       `json:"diastolic"  db:"diastolic" binding:"required"`
+	Heartrate json.Number       `json:"heartrate"  db:"heartrate" binding:"required"`
 }
 
 // String is not required by pop and may be deleted
@@ -33,15 +32,16 @@ func (e Entries) String() string {
 	return string(je)
 }
 
-// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
-// This method is not required and may be deleted.
-func (e *Entry) Validate(tx *pop.Connection) (*validate.Errors, error) {
-	// return validate.NewErrors(), nil
-	return validate.Validate(
-		&validators.IntIsPresent{Field: e.Systolic, Name: "Systolic"},
-		&validators.IntIsPresent{Field: e.Diastolic, Name: "Diastolic"},
-		&validators.IntIsPresent{Field: e.Heartrate, Name: "Heartrate"},
-	), nil
+func (e *Entry) IsValid(errors *validate.Errors) {
+	if e.Systolic == "" {
+		errors.Add("Systolic", "is required")
+	}
+	if e.Diastolic == "" {
+		errors.Add("Diastolic", "is required")
+	}
+	if e.Heartrate == "" {
+		errors.Add("Heartrate", "is required")
+	}
 }
 
 // ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
